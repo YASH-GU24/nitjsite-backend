953ad6c35616ccc2584e889082298c5470b07f89
YASH-GU24 <yashg.it.20@nitj.ac.in>
2023-04-11 17:06:02 +0530

    Revert "Merge pull request #76 from YASH-GU24/teamcchangs"
    This reverts commit 53b1d8095b7815a148bc25a090c06478fa715e4b, reversing
    changes made to 695b7a7cf7f42719d0c4dee3c87c94c2bc796925.

--- models/departmentClubs.js
+++ models/departmentClubs.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose');
 const Schema=new mongoose.Schema({
     department:{
         type:String,
-        enum: ['bt', 'ch', 'cy', 'ce', 'cse', 'ee', 'ece', 'hm', 'ipe', 'it', 'ice', 'ma', 'me', 'ph', 'tt', 'cf','cee','cai']
+        enum: ['bt', 'ch', 'cy', 'ce', 'cse', 'ee', 'ece', 'hm', 'ipe', 'it', 'ice', 'ma', 'me', 'ph', 'tt', 'cf']
     },
     name:{
       type:String
@@ -12,17 +12,19 @@ const Schema=new mongoose.Schema({
     img: {type:String},
     type:{
       type:String,
-      enum:["Club","Team","Societies"]
+      enum:["Club","Coordinator","Team","Societies"]
     },
     description: { type: String, default: "" },
     show: { type: Boolean, default: true },
-    FacebookUrl: { type: String },
-    InstagramUrl: { type: String },
-    TwitterUrl: { type: String },
-    LinkedinUrl: { type: String },
     sourceOfInfo: {
-      type:  String
-  },
+        type: Object,
+        default: {
+          name: null,
+          email: null,
+          designation: null,
+          department: null,
+        }
+      }
 },{timestamps:true})
 
 module.exports=mongoose.model("departmentClubs",Schema);
